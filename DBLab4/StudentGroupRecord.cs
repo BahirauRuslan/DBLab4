f5028fe51280abcf792e21661181514a600e412b
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-03-14 13:32:32 +0300

    Refactoring complete

--- DBLab4/StudentGroupRecord.cs
+++ DBLab4/StudentGroupRecord.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DBLab4
+﻿namespace DBLab4
 {
     class StudentGroupRecord
     {
